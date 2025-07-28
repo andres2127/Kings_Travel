400ae9b52a9c95fa9c56d1339d57914664edc3ec
Andres <osquitarsanchez48@gmail.com>
2025-07-28 12:31:23 -0500

    HTML and CSS finished

--- travelRecommendation/Contact_Us/contactUs.js
+++ travelRecommendation/Contact_Us/contactUs.js
@@ -1,18 +1,3 @@
-let lastScrollTop = 0;
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', function () {
-let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-    if (currentScroll > lastScrollTop) {
-      // Scrolling down
-      nav.style.top = "-70px"; // Oculta el nav (ajusta según tu altura)
-    } else {
-      // Scrolling up
-      nav.style.top = "0";
-    }
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
-
 document.getElementById('formularioContacto').addEventListener('submit', function(event) {
     event.preventDefault(); // evita recargar la página
 
